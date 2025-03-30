b746e6219a29666a71d30bf7ca33ae43d2abc6df
codechini <arangathvenuchinmay@gmail.com>
2025-03-30 12:23:30 +0530

    added redirects for netlify

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
-    <div>
+    <div className="w-full h-screen flex flex-col items-center justify-center bg-gray-50">
       <h1>404 - Page Not Found</h1>
       <p>The page youre looking for doesnt exist.</p>
+      <Link className="bg-lime-200" to="/">Home</Link>
     </div>
   );
 };
